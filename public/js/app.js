c664e82d61de7a881c5873ca782e6a5fd72290a8
psloss <psloss@belltec.com>
2014-11-11 16:21:48 -0600

    initial commit with .gitignore & more files

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+// Declare app level module which depends on filters, and services
+
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+  config(function ($routeProvider, $locationProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'partials/index',
+        controller: IndexCtrl
+      }).
+      when('addPost', {
+        templateUrl: 'partials/addPost',
+        controller: AddPostCtrl
+      }).
+      when('/readPost/:id', {
+        templateUrl: 'partials/readPost',
+        controller: ReadPostCtrl
+      }).
+      when('/editPost/:id', {
+        templateUrl: 'partials/editPost',
+        controller: EditPostCtrl
+      }).
+      when('/deletePost/:id', {
+        templateUrl: 'partials/deletePost',
+        controller: DeletePostCtrl
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+    $locationProvider.html5Mode(true);
+  }]);
